4df318a5a3bb5c7bfb6006a4d84a1de300b69d77
Caio Victor <caiovictor.pcb@gmail.com>
2021-11-15 21:19:06 -0300

    get args from message

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Client } from "discord.js";
+import { Client, ClientUser, Role, User } from "discord.js";
 import dotenv from "dotenv";
 import axios from "axios";
 
@@ -8,12 +8,25 @@ const bot = new Client();
 
 const PREFIX = process.env.PREFIX;
 
+
 bot.on("ready", () => {
     console.log("XQDL BOT está Online!!");
 });
 
-bot.on("message", message => {
-    console.log(message)
-});
+
+
+bot.on("message", (message) => {
+    if(message.author.bot) return;
+    if(message.mentions.has('805104755658457108')) message.reply('ME MARCOU');
+    const mensagem = message.content.split(' ')
+    const comando = mensagem[1]
+    const discordId = message.author.id
+    const nick = mensagem[mensagem.length - 2]
+    const platform = mensagem[mensagem.length - 1]
+    console.log(discordId)
+    console.log(comando)
+    console.log(nick)
+    console.log(platform)
+  });
 
 bot.login(process.env.BOT_TOKEN);
