263b0349158adb94b05b38a6f3253579bed9dd31
Caio Victor <caiovictor.pcb@gmail.com>
2021-11-24 12:29:49 -0300

    done

--- src/utils/generateEmbedStats.ts
+++ src/utils/generateEmbedStats.ts
@@ -1,6 +1,7 @@
 import { ApiResponse} from "../types/apiresponse.types";
 import { MessageEmbed } from "discord.js";
 import Arredondar from './arrendodarKD'
+
 export const generateStatsEmbed = (player:ApiResponse) => {
     const Embed = new MessageEmbed()
         .setColor('#0099ff')
@@ -8,17 +9,23 @@ export const generateStatsEmbed = (player:ApiResponse) => {
         .setAuthor('Teus Stats')
         .setThumbnail(player.avatar_url_256)
         .addFields(
-            { name: 'General',
-            value: `Kills: ${player.stats.general.kills} \n
-                    Deaths: ${player.stats.general.deaths} \n
-                    KD: ${Arredondar.round(player.stats.general.kd, -1)}
-            `},
-            { name: '\u200B', value: '\u200B' },
-            { name: 'Inline field title', value: 'Some value here', inline: true },
-            { name: 'Inline field title', value: 'Some value here', inline: true },
+
+            { name: 'Sobre',
+            value: `Level: ${player.progression.level}
+                    Playtime: ${player.stats.general.playtime/3600} horas
+            `,
+            inline: true
+            },
+
+            { name: 'Geral',
+            value: `Kills: ${player.stats.general.kills}
+                    Deaths: ${player.stats.general.deaths}
+                    KD: ${player.stats.general.kd}
+            `,
+            inline:true
+            },
         )
-        .addField('Inline field title', 'Some value here', true)
-        .setImage('https://i.imgur.com/1YXeT6M.png')
+        .setImage(player.avatar_url_256)
         .setTimestamp()
         .setFooter('Stats tirados diretamente do XQDL BOT', 'https://i.imgur.com/1YXeT6M.png');
         return Embed
